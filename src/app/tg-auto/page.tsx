78188336b0f61a3c5b6db27ed426fb61fe83703e
newTT <qq>
2024-02-20 23:55:34 +0800

    feat: 🎸 load tg

---
+++ src/app/tg-auto/page.tsx
@@ -0,0 +1,15 @@
+import { telegramAuthAccessToken } from "@/utils/telegram";
+import React, { useEffect } from "react";
+
+export default function Auto() {
+  useEffect(() => {
+    setTimeout(() => {
+      telegramAuthAccessToken({
+        botUsername: "portkey_connect_bot",
+        authCallbackUrl:
+          "https://aa-portkey.portkey.finance/api/app/telegramAuth/receive",
+      });
+    }, 1000);
+  }, []);
+  return <div>Auto</div>;
+}
